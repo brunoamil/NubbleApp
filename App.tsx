ce875c0c2cbdd03ba745d162fc9cb0566a467df7
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2023-08-30 07:11:44 -0300

    feat: configuration restyle, create component Text, Box and Button

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 import {Text} from './src/components/Text/Text';
+import {Button} from './src/components/Button/Button';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text preset="headingLarge" italic bold>
-        My App
-      </Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge" italic>
+            NublbleApp
+          </Text>
+          <Button marginBottom="s20" title="Entrar" />
+          <Button loading title="Loading" />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {ActivityIndicator} from 'react-native';
+
+import {Text} from '../Text/Text';
+import {TouchableOpacityBox, TouchableOpacityBoxProps} from '../Box/Box';
+
+interface ButtonProps extends TouchableOpacityBoxProps {
+  title: string;
+  loading?: boolean;
+}
+export function Button({
+  title,
+  loading,
+  ...touchableOpacityBoxProps
+}: ButtonProps) {
+  return (
+    <TouchableOpacityBox
+      backgroundColor="buttonPrimary"
+      paddingHorizontal="s20"
+      height={50}
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="s16"
+      {...touchableOpacityBoxProps}>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text preset="paragraphMedium" bold color="primaryContrast">
+          {title}
+        </Text>
+      )}
+    </TouchableOpacityBox>
+  );
+}
