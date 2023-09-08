bd62c18e0f7522cb148761404027e7d748a2fd32
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2023-09-08 09:33:39 -0300

    feat: create new screen for login - ui

--- App.tsx
+++ App.tsx
@@ -1,36 +1,42 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, TextInput, View} from 'react-native';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 
 import {Text} from './src/components/Text/Text';
 import {Box} from './src/components/Box/Box';
 import {Icon} from './src/components/Icon/Icon';
+import {Button} from './src/components/Button/Button';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text preset="headingLarge" italic>
-            NublbleApp
+          <Text marginBottom="s8" preset="headingLarge">
+            Olá
           </Text>
-          <Box flexDirection="row">
-            <Icon name="chevronRight" size={50} />
-            <Icon name="heartFill" color="buttonPrimary" size={50} />
-            <Icon name="profile" size={50} />
-            <Icon name="profileFill" size={50} />
-            <Icon name="heart" size={50} />
-            <Icon name="bellOn" color="carrotSecondary" size={50} />
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <Box mb="s20">
+            <TextInput
+              placeholder="Digite seu e-mail"
+              style={{borderWidth: 1, height: 50}}
+            />
           </Box>
-          <Box flexDirection="row">
-            <Icon name="newPost" size={50} />
-            <Icon name="camera" size={50} />
-            <Icon name="chat" size={50} />
-            <Icon name="chatOn" color="error" size={50} />
-            <Icon name="flashOff" size={50} />
-            <Icon name="flashOn" size={50} />
+          <Box>
+            <TextInput
+              placeholder="Digite sua senha"
+              style={{borderWidth: 1, height: 50}}
+            />
           </Box>
+          <Text preset="paragraphSmall" color="primary" bold mt="s10">
+            Esqueci minha senha
+          </Text>
+
+          <Button title="Entrar" mt="s48" />
+          <Button title="Criar uma conta" mt="s12" preset="outline" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
