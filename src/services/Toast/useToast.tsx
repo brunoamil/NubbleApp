7a2ee0b3f8d91fecaf31d8a4585a35a47a0b2164
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2023-09-19 21:40:07 -0300

    feat: configuration zustand, create context

--- src/services/Toast/useToast.tsx
+++ src/services/Toast/useToast.tsx
@@ -1,6 +1,23 @@
 import {ToastService} from './types';
-import {useToastContext} from './useToastContext';
+// import {useToastContext} from './useToastContext';
+import {useToastServiceZustand, useToastZustand} from './useToastZustand';
+// import {useToastZustand} from './useToastZustand';
 
-export function useToast(): ToastService {
-  return useToastContext();
+export function useToast(): ToastService['toast'] {
+  // WITH CONTEXT
+  // const {toast} = useToastContext();
+  return useToastZustand();
+}
+
+export function useToastService(): Pick<
+  ToastService,
+  'showToast' | 'hideToast'
+> {
+  // WITH CONTEXT
+  // const {showToast, hideToast} = useToastContext();
+  // return {
+  //   showToast,
+  //   hideToast,
+  // };
+  return useToastServiceZustand();
 }
