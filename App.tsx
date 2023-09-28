a2be885866d9d5c0a926a58e94d1f9a23d778f1d
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2023-09-27 22:53:19 -0300

    feat: install configuration MMKV

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import {AuthCredentialsProvider, ToastProvider} from '@services';
+import {
+  AuthCredentialsProvider,
+  MMKVStorage,
+  ToastProvider,
+  initilizeStorage,
+} from '@services';
 import {ThemeProvider} from '@shopify/restyle';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -11,6 +16,9 @@ import {Router} from './src/routes/Routes';
 import {theme} from './src/theme/theme';
 
 const queryClient = new QueryClient();
+
+initilizeStorage(MMKVStorage);
+
 function App(): JSX.Element {
   return (
     <AuthCredentialsProvider>
